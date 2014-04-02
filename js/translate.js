bf4f57804f94c9964ad1326a40e9bfcb49d920ce
Anthony Chin <wei-wing.chin@mail.mcgill.ca>
2014-04-01 23:13:59 -0400

    work both ways

--- js/translate.js
+++ js/translate.js
@@ -1,15 +1,60 @@
 // function update () {
-//   var albhedToEnglish = document.getElementById('albhed-to-english');
-//   var englishToAlBhed = document.getElementById('english-to-albhed');
-//   var tmp = albhedToEnglish.value;  
-//   englishToAlBhed.value = tmp;
+//   var albhed = document.getElementById('albhed');
+//   var english = document.getElementById('english');
+//   var tmp = albhed.value;  
+//   english.value = tmp;
 // } 
 
+// function update2 () {
+//   var albhed = document.getElementById('albhed');
+//   var english = document.getElementById('english');
+//   var tmp = english.value;  
+//   albhed.value = tmp;
+// }
+
 $(document).ready(function() {
-  $('#albhed-to-english').keyup(function(){
-    var albhedToEnglish = $('#albhed-to-english').val();
-    var englishToAlBhed = $('#english-to-albhed').val();
-    var alBhedPhabet = 
-    $('#english-to-albhed').html(albhedToEnglish);
+  $('#albhed').keyup(function(){
+    var albhed = $('#albhed').val();
+    //var alBhedPhabet = getAlphabet(albhedToEnglish, 1);
+    $('#english').val(albhed);
   });
+
+  $('#english').keyup(function(){
+    var english = $('#english').val();
+    //var alBhedPhabet = getAlphabet(albhedToEnglish, 1);
+    $('#albhed').val(english);
+  });  
 });
+
+// var letter = {
+//   A = 'Y',
+//   B = 'P',
+//   C = 'L',
+//   D = 'T',
+//   E = 'A',
+//   F = 'V',
+//   G = 'K',
+//   H = 'R',
+//   I = 'E',
+//   J = 'Z',
+//   K = 'G',
+//   L = 'M',
+//   M = 'S',
+//   N = 'H',
+//   O = 'U',
+//   P = 'B',
+//   Q = 'X',
+//   R = 'N',
+//   S = 'C',
+//   T = 'D',
+//   U = 'I',
+//   V = 'J',
+//   W = 'F',
+//   X = 'Q',
+//   Y = 'O',
+//   Z = 'W'
+// }
+
+// function getAlphabet(toDecipher, direction) {
+
+// }
